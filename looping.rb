96c3c0844bce4a6d3afcbbe5f581f43bcb05a482
scott.waring@gmail.com <scott.waring@gmail.com>
2018-08-08 17:43:02 +0000

    automatically backed up by learn

--- looping.rb
+++ looping.rb
@@ -1,5 +1,12 @@
 def looping
-	#your code here
+  counter = 0 
+	puts "This is a silly lesson"
+	while counter < 5
+	counter += 1
+	end
+	if counter == 4
+	  break
+	 end
 end
 
 #call your method here
